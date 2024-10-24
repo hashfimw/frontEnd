660f361753ea1fa223113469db3f0e29081606c7
hashfimw <hashfimawarid@Hashfis.local>
2024-10-24 11:33:27 +0700

    update day4

---
+++ react-tailwind/src/App.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+
+function App() {
+  return (
+    <div>
+      <div className=" bg-slate-600 md:bg-orange-400 hover:bg-blue-300 h-[60px] md:h-[100px] 
+      flex justify-between items-center text-white fixed top-0 w-[100vw]">
+        Navbar
+        <div>Navbar</div>
+        <div>Navbar</div>
+        </div>
+    </div>
+ 
+  );
+}
+
+export default App
