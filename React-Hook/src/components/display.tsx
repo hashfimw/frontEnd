c5ed497cc5b2f3b8aabceec1693a7af7f513c800
hashfimw <hashfimawarid@Hashfis.local>
2024-10-29 15:59:02 +0700

    29oct

--- React-Hook/src/components/display.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { UserContext } from "../App";
-
-function DisplayComp() {
-  const user = useContext(UserContext);
-  return <h1>Halo {user}</h1>;
-}
-
-export default DisplayComp;
